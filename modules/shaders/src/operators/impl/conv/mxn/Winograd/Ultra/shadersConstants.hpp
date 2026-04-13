07df787525246e5881b8ecf7a8f8dcd64eb308b1
Julien Fleuret <julien.fleuret@amd.com>
2026-04-12 21:11:03 -0400

    Failed attempt to integrate Winograd Ultra

---
+++ modules/shaders/src/operators/impl/conv/mxn/Winograd/Ultra/shadersConstants.hpp
@@ -0,0 +1,9 @@
+/* Copyright (c) 2025 Advanced Micro Devices, Inc. All rights reserved. */
+#pragma once
+
+#include "core/core.hpp"
+
+namespace mlss::conv::mxn::winograd::ultra::fp16
+{
+
+} // namespace mlss::conv::mxn::winograd::ultra::fp16
